51944254d897cb400c11ff0fef1762c81ee932d8
buktop72 <jasonx2007@yandex.ru>
2021-04-25 11:07:59 +0400

    create main

---
+++ lesson_1/application/salary.py
@@ -0,0 +1,3 @@
+def calculate_salary():
+    print('Расчет зарплаты сотрудников')
+
---
+++ lesson_1/main.py
@@ -0,0 +1,32 @@
+"""
+1. Разработать структуру программы "Бухгалтерия".
+main.py;
+директория application:
+-- salary.py;
+-- директория db:
+--- people.py;
+main.py - основной модуль для запуска программы.
+В модуле salary.py функция calculate_salary.
+В модуле people.py функция get_employees.
+2. Импортировать функции в модуль main.py и вызывать эти функции в конструкции.
+if __name__ == '__main__':
+Сами функции реализовать не надо. Достаточно добавить туда какой-либо вывод.
+3. Познакомиться с модулем datetime. При вызове функций модуля main.py выводить текущую дату.
+4. Создать рядом с файлом main.py модуль dirty_main.py и импортировать все функции с помощью конструкции 
+(необязательное задание)
+"""
+from datetime import date
+from application import salary as s
+from application import people as p
+
+
+def main():
+    print(date.today())
+    s.calculate_salary()
+    p.get_employees()
+
+
+
+
+if __name__ == '__main__':
+    main()
