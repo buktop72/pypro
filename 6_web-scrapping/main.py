9ac38d32baccc694423c1d2e890c81df505827d8
buktop72 <jasonx2007@yandex.ru>
2021-05-13 21:46:56 +0400

    дополнительное задание web-scrapping

--- 6_web-scrapping/main.py
+++ 6_web-scrapping/main.py
@@ -7,6 +7,9 @@
 # определяем список ключевых слов
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 # Ваш код
+Дополнительное (необязательное) задание
+Улучшить скрипт так, чтобы он анализировал не только preview-информацию статьи, но и весь текст статьи целиком.
+Для этого потребуется получать страницы статей и искать по тексту внутри этой страницы.
 """
 import requests
 from bs4 import BeautifulSoup
@@ -33,8 +36,20 @@ def scrap(keywords, url):
         text_set = set()
         for i in text_block: 
             for j in i.split():
-                text_set.add(re.search('\w*', j).group(0).lower()) # преобразуем в нижний регистр и избавляемся от спецсимволов 
-        text_set = text_set | tags_set | title_set # объединение всех множеств одного блока
+                text_set.add(re.search('\w*', j).group(0).lower()) # преобразуем в нижний регистр и избавляемся от спецсимволов
+# Дополнительное задание       
+        href_page = article.find('a', class_="btn btn_x-large btn_outline_blue post__habracut-btn").attrs.get('href') # ссылка на страницу
+        request_page = requests.get(href_page)
+        if not request_page.ok:
+            raise ValueError('Site not found')
+        soup_page = BeautifulSoup(request_page.text, 'html.parser') # экземпляр класса BS, HTML
+        text_page = soup_page.find_all('p') # Получаем параграфы статьи
+        text_page_set = set()
+        for i in text_page: 
+            for j in i.text.split(): 
+                text_page_set.add(re.search('\w*', j).group(0).lower()) # преобразуем в нижний регистр, избавляемся от спецсимволов, добавляем в set
+
+        text_set = text_set | tags_set | title_set | text_page_set # объединение всех множеств одного блока
         if disaire_hub & text_set:            
             href = article.find('h2', class_="post__title").find('a').attrs.get('href') 
             print('Совпадение по тэгу ', str(disaire_hub & text_set)) # тэг
