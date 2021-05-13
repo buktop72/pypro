fe5db86da65c7ac9babbae5fd5e0fbfb51828c20
buktop72 <jasonx2007@yandex.ru>
2021-05-13 20:16:06 +0400

    переписал  6_web-scrapping/main.py (re)

--- 6_web-scrapping/main.py
+++ 6_web-scrapping/main.py
@@ -10,6 +10,7 @@ KEYWORDS = ['дизайн', 'фото', 'web', 'python']
 """
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def scrap(keywords, url):
     disaire_hub = set(keywords)
@@ -19,27 +20,27 @@ def scrap(keywords, url):
     soup = BeautifulSoup(request.text, 'html.parser') # экземпляр класса BS, HTML
     for article in soup.find_all('article'):
         title = article.find('h2', class_="post__title") # Получаем заголовки статей
+        # print(title.text)
         title_set = set()
         for i in title.text.strip().split():
-            title_set.add(i.lower().strip(':".,!?()«»'))  # преобразуем в нижний регистр и избавляемся от спецсимволов
+            title_set.add(re.search('\w*', i).group(0).lower())  # преобразуем в нижний регистр и избавляемся от спецсимволов
         tags = article.find_all('ul', class_="post__hubs inline-list") # Получаем список тэгов статей
         tags_set = set()
         for i in tags: # преобразуем в множество
             for j in i.text.split():
-                tags_set.add(j.lower().strip(':".,!?()«»')) # преобразуем в нижний регистр и избавляемся от спецсимволов    
+                tags_set.add(re.search('\w*', j).group(0).lower()) # преобразуем в нижний регистр и избавляемся от спецсимволов    
         text_block = [t.text for t in article.find_all('p')] # получаем текст блока   
         text_set = set()
         for i in text_block: 
             for j in i.split():
-                text_set.add(j.lower().strip(':".,!?()«»')) # преобразуем в нижний регистр и избавляемся от спецсимволов 
+                text_set.add(re.search('\w*', j).group(0).lower()) # преобразуем в нижний регистр и избавляемся от спецсимволов 
         text_set = text_set | tags_set | title_set # объединение всех множеств одного блока
         if disaire_hub & text_set:            
             href = article.find('h2', class_="post__title").find('a').attrs.get('href') 
             print('Совпадение по тэгу ', str(disaire_hub & text_set)) 
             print(article.find('span', class_="post__time").text)
             print(title.text.strip())
-            print(href)
-            print()
+            print(href, '\n')   
 
 if __name__ == '__main__':
     KEYWORDS = ['дизайн', 'фото', 'web', 'python']
